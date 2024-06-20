c6f9b29b57857bcf4450b97802236b37f0dfea14
Guillaume <103214559+charlieGui@users.noreply.github.com>
2024-06-20 22:41:58 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'guimix/abcdtaxi',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'calcinai/php-imagick' => array(
+            'pretty_version' => 'v0.1.2',
+            'version' => '0.1.2.0',
+            'reference' => '001530b19560b9862ffe78c3ae29ad5dc2549e6d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../calcinai/php-imagick',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'guimix/abcdtaxi' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
