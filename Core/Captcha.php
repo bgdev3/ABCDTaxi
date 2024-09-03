f4ff62d71ba79e5465baa0c7a5b815753a6bac93
Guillaume <103214559+charlieGui@users.noreply.github.com>
2024-09-03 23:39:13 +0200

    Update Captcha.php

--- Core/Captcha.php
+++ Core/Captcha.php
@@ -14,7 +14,7 @@ class Captcha {
      {
         // Paramètre la requête POST
         $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-        $recaptcha_secret = '6LebG6MpAAAAAByZ4w9GQaDKTfkaPeo6vBO8UqII';
+        $recaptcha_secret = '**********';
         $recaptcha_response = $secret;
 
         // Récupère et decode le fetch POST
