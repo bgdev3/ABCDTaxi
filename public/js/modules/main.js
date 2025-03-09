e4a1fa0b292938199c0169b413b7fbd65c2c052c
Guillaume <103214559+bgdev3@users.noreply.github.com>
2025-03-09 12:05:13 +0100

    Add files via upload

---
+++ public/js/modules/main.js
@@ -0,0 +1,82 @@
+
+// Charge les modules pour la page d'accueil
+window.addEventListener('DOMContentLoaded', async ()=>{
+    const slideShow = await import("./modules/slideShow.js");
+    const manageMenu = await import("./modules/menuManager.js");
+    const scrollTop = await import("./modules/scrollTop.js");
+    const footerMap = await import("./modules/LocationMap.js");
+    // Rend invisible le loader lorsque la page est chargé
+    document.querySelector('.loader-container').classList.add('loader-hide');
+    document.querySelector('.loader').classList.add('loader-hide');
+    
+    // Execute les modules
+    slideShow.slideShow(), manageMenu.menuManager(), scrollTop.scrollToTop(), footerMap.initMapFooter();
+})
+
+
+let book = document.querySelector('div.date');
+let getMap = document.querySelector('#map');
+let getFooterMap = document.querySelector('#map-footer');
+let customer = document.querySelector('.table-responsive');
+let getForm = document.querySelector('form');
+let searchTransport = document.querySelector('#searchTransport');
+
+// Page Reservation
+if(book != 'undefined'){
+    async function slides() {
+        const date = await import("./modules/dateReservation.js");
+        date.dateReservation();
+    }
+    slides();
+}
+
+// Page Map et itinéraire
+if(getMap){
+    async function map() {
+        const date = await import("./modules/googleMap.js");
+        date.initMap();
+    }
+    map();
+}
+
+// if(getFooterMap){
+   
+//     async function footerMap() {
+       
+//         const date = await import("./modules/LocationMaps.js");
+//         date.initMapFooter();
+//     }
+//     footerMap();
+// }
+
+// Pages des tableaux
+if(customer) {
+    async function tableResponsive() {
+        const date = await import("./modules/reservationResponsive.js");
+        date.responsiveArray();
+    }
+    tableResponsive();
+}
+
+// Pages des fomrulaires
+if(getForm) {
+   
+    async function form() {
+        const date = await import("./modules/verif.js");
+        date.verif();
+    }
+    form();
+}
+
+// Page admin
+if(searchTransport) {
+    async function search() {
+        const date = await import("./modules/getHistory.js");
+        date.getHistory();
+    }
+    search();
+}
+
+
+
+ 
